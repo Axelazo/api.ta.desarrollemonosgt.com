b909e9293ae3faa72a6a004b34bb937dd88e2acf
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2024-03-25 13:38:21 -0600

    chore: environment configuration file reordering

--- src/configuration/environment.ts
+++ src/configuration/environment.ts
@@ -3,22 +3,20 @@ import path from "path";
 
 config();
 
+// APP
 export const PORT = parseInt(process.env.PORT as string, 10);
-
 export const SRC_PATH = path.join(__dirname, ".."); // ONE level up is /src
 
 // DB
-
 export const DB_PORT = process.env.DB_PORT;
-
 export const DB_USERNAME = process.env.DB_USERNAME;
-
 export const DB_PASSWORD = process.env.DB_PASSWORD;
-
 export const DB_DATABASE_NAME = process.env.DB_DATABASE_NAME;
-
 export const DB_HOSTNAME = process.env.DB_HOSTNAME;
-
 export const DB_DIALECT = process.env.DB_DIALECT;
-
 export const DB_DEV = process.env.DB_DEV;
+
+// AUTH
+export const AUTH_SECRET = process.env.AUTH_SECRET;
+export const AUTH_EXPIRES = process.env.AUTH_EXPIRES;
+export const AUTH_ROUNDS = process.env.AUTH_ROUNDS;
